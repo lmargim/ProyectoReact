52f5048dcbb82743ee4c8f21150ef7363826eef2
Luis Martín <xlmargim360@ieshnosmachado.org>
2025-02-21 10:50:52 +0100

    Añadimos enlace de despliegue en cors

--- backend_agora/index.js
+++ backend_agora/index.js
@@ -23,7 +23,10 @@ app.use(express.json());
 // Configurar CORS para admitir solicitudes desde http://localhost:5173
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://agora-production-ef4f.up.railway.app"
+    ],
     credentials: true,
   })
 );
